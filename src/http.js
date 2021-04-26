79f7b74dfb7a1b71a1826e2ca481d6e1eca5e45a
Anton Smolko <asmolko@yandex.ru>
2021-04-26 23:13:32 +0300

    debug project

--- src/http.js
+++ src/http.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
+import i18next from './i18next.js';
 
 export default (url) => axios
-  .get(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`, { timeout: 10000 })
-  .then((response) => response);
+  .get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`, { timeout: 10000 })
+  .catch(() => {
+    throw new Error(i18next.t('errors.network_error'));
+  });
