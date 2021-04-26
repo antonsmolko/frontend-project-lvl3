e27d1e0f00690c65df1fcb29084f53aa57ef14cc
Anton Smolko <asmolko@yandex.ru>
2021-04-26 22:56:48 +0300

    debug project

--- src/http.js
+++ src/http.js
@@ -6,14 +6,16 @@ const request = {
     const _get = new URL('/get', 'https://hexlet-allorigins.herokuapp.com');
 
     _get.searchParams.set('url', url);
-    _get.searchParams.set('disableCache', true);
+    _get.searchParams.set('disableCache', 'true');
 
     return _get.toString();
   }
 }
 
 export default (url) => axios
-  .get(request.build(url), { timeout: 10000 })
+  .get(request.build(url))
   .catch((e) => {
+    console.log('url', url);
+    console.log('error', e);
     throw new Error(i18next.t('errors.network_error'));
   });
