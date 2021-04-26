3d60c1a876a5e391d844f39200f97eab76074bc1
Anton Smolko <asmolko@yandex.ru>
2021-04-26 23:03:15 +0300

    debug project

--- src/http.js
+++ src/http.js
@@ -1,21 +1,8 @@
 import axios from 'axios';
 import i18next from './i18next.js';
 
-const request = {
-  build: (url) => {
-    const _get = new URL('/get', 'https://hexlet-allorigins.herokuapp.com');
-
-    _get.searchParams.set('url', url);
-    _get.searchParams.set('disableCache', 'true');
-
-    return _get.toString();
-  }
-}
-
 export default (url) => axios
-  .get(request.build(url))
-  .catch((e) => {
-    console.log('url', url);
-    console.log('error', e);
+  .get(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`, { timeout: 10000 })
+  .catch(() => {
     throw new Error(i18next.t('errors.network_error'));
   });
