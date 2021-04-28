73673971006b73d236924a6ed2cf15602f837ab9
Anton Smolko <asmolko@yandex.ru>
2021-04-28 23:09:44 +0300

    debug project

--- src/http.js
+++ src/http.js
@@ -4,5 +4,5 @@ import i18next from './i18next.js';
 export default (url) => axios
   .get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`, { timeout: 10000 })
   .catch(({ message }) => {
-    throw new Error('message');
+    throw new Error(message);
   });
