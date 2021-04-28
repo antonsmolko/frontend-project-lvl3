6f272cc3a93366bf022fc04bff8f35d240e423ba
Anton Smolko <asmolko@yandex.ru>
2021-04-28 23:31:15 +0300

    debug project

--- src/http.js
+++ src/http.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import i18next from './i18next.js';
 
 export default (url) => axios
-  .get('https://hexlet-allorigins.herokuapp.com/get?url=https%3A%2F%2Fru.hexlet.io%2Flessons.rss&disableCache=true', { timeout: 10000 })
+  .get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}&disableCache=true`, { timeout: 10000 })
+  .then((response) => {
+    console.log('response', response)
+  })
   .catch((error) => {
     console.log(error)
     throw new Error(i18next.t('errors.network_error'));
