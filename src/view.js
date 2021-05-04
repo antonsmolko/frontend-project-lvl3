6feb319d15290140f20bfb9c17863a95be323ea1
Anton Smolko <asmolko@yandex.ru>
2021-05-04 15:49:36 +0300

    debug

--- src/view.js
+++ src/view.js
@@ -2,27 +2,7 @@ import onChange from 'on-change';
 import render from './render.js';
 import { renderMessage, setInputForm } from './helpers.js';
 
-const state = {
-  form: {
-    url: '',
-    isValid: false,
-  },
-  process: {
-      state: 'filling',
-      watched: false,
-  },
-  message: {
-      success: false,
-      body: '',
-  },
-  rss: {
-      feeds: [],
-      posts: [],
-      readPosts: new Set(),
-  },
-};
-
-const watchedState = onChange(state, (path, value) => {
+export default (state) => onChange(state, (path, value) => {
   const feedback = document.querySelector('.feedback');
   const formInput = document.querySelector('input[name="url"]');
   const submitButton = document.querySelector('button[type="submit"]');
@@ -40,5 +20,3 @@ const watchedState = onChange(state, (path, value) => {
     renderMessage(feedback, formInput, state.message.success, value);
   }
 });
-
-export default watchedState;
